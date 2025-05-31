602bc94fb5e9410d1a1f219d1984c0efe215c0b3
Lokesh Baviskar <101720181+22karlokesh@users.noreply.github.com>
2025-05-31 22:30:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,17 @@ const getSumBtn = document.createElement("button");
 getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
+let table = document.getElementById("table")
 const getSum = () => {
 //Add your code here
+	let sum=0;
+	const allPrice = document.querySelectorAll(.price);
+	for(int i =0;i<allPrice.length;i++){
+		sum = sum+parseInt(allPrice[i]);
+	}
+
+	let newElement = document.createElement("tr");
+	newElement.innerHTML = sum;
   
 };
 
